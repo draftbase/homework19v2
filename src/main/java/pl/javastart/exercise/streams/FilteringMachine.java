6bdc79eef30fe56100cad94f070f868f0c1d153d
draftbase <55540514+draftbase@users.noreply.github.com>
2019-11-30 15:15:04 +0100

    [HW] - zajęcia 19, zadanie 1

--- src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
@@ -1,23 +1,40 @@
 package pl.javastart.exercise.streams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FilteringMachine {
 
     public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
-        return null;
+        List<Integer> list = numberList.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+        return list;
     }
 
     public List<Integer> filterOutLowerNumbersThan20(List<Integer> numberList) {
-        return null;
+        List<Integer> list = numberList.stream()
+                .filter(n -> n>20)
+                .collect(Collectors.toList());
+        return list;
     }
 
     public List<Book> convertToBooks(List<String> titles) {
-        return null;
+        List<Book> books = titles.stream()
+                .map(Book::new)
+                .collect(Collectors.toList());
+        return books;
     }
 
     // Metoda powinna przekształcić tytuły na książki i zwrócić tylko te które rozpoczynają się od słowa "Gra"
     public List<Book> convertToBooksAndReturnOnlyStartingWithGra(List<String> titles) {
-        return null;
+        List<Book> books = titles.stream()
+                .filter(f -> f.startsWith("Gra"))
+                .map(Book::new)
+                .collect(Collectors.toList());
+        return books;
     }
 }
--- src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
@@ -1,15 +1,24 @@
 package pl.javastart.exercise.streams;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class FilteringMachineTwo {
 
     // metoda powinna zwrócić imiona wszystkich niedorosłych osób (mniej niż 18 lat)
     public List<String> findKidNames(List<Person> ppl) {
-        return null;
+        List<String> names = ppl.stream()
+                .filter(f -> f.getAge() < 18)
+                .map(Person::getName)
+                .collect(Collectors.toList());
+        return names;
     }
 
     public List<User> convertPeopleToUsers(List<Person> people) {
-        return null;
+        List<User> users = people.stream()
+                .map(person -> new User(person.getName(), person.getAge(), person.getName()+"_"+person.getAge()))
+                .collect(Collectors.toList());
+        return users;
     }
 }
